60139e5efa69da6ec3650dd75ced25ca54d03a4b
Jared Davidson (Archetapp) <archetapp@gmail.com>
2021-09-07 17:29:05 -0600

    update

--- Sources/SwiftUIView.swift
+++ Sources/SwiftUIView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 public struct SwiftUIView: View {
+	public init() {}
 	public var body: some View {
 		Text("THIS IS THE SWIFTUIVIEW FROM THE PACKAGE")
 	}
