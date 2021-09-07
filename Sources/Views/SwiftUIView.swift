8b4f1aae8c0fd12a0609f340f848d126d8a334f6
Jared Davidson (Archetapp) <archetapp@gmail.com>
2021-09-07 17:37:29 -0600

    update

--- Sources/Views/SwiftUIView.swift
+++ Sources/Views/SwiftUIView.swift
@@ -13,6 +13,9 @@ public struct SwiftUIView: View {
 	public init() {}
 	public var body: some View {
 		Text("THIS IS THE SWIFTUIVIEW FROM THE PACKAGE")
-		Image("starscream")
+		Image(uiImage: UIImage(named: "starscream", in: .module, with: nil)!)
+			.resizable()
+			.frame(width: 100, height: 100, alignment: .center)
+			.aspectRatio(contentMode: .fit)
 	}
 }
