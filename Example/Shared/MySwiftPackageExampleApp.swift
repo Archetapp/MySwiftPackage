64dbe208c7d0162aefa3ed2ade2de51c20a09668
Jared Davidson (Archetapp) <archetapp@gmail.com>
2021-09-07 17:23:26 -0600

    update

---
+++ Example/Shared/MySwiftPackageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftPackageExampleApp.swift
+//  Shared
+//
+//  Created by Jared Davidson on 9/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftPackageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
