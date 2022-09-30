b7a220d212e081032092436ab84938a94d4ca9be
Jared Davidson (Archetapp) <archetapp@gmail.com>
2022-09-30 13:15:54 -0600

    fix

--- Package.swift
+++ Package.swift
@@ -18,6 +18,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "MySwiftPackage",
+                path: "Sources"),
         .testTarget(
             name: "MySwiftPackageTests",
             dependencies: ["MySwiftPackage"]),
